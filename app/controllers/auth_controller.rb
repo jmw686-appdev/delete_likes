70f6ee4390a51187fb6d63c236aa9162a85e6f89
sun-vm <jmw686@nyu.edu>
2018-09-09 15:58:20 -0500

    add figaro gem and config some tokens

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,11 @@
 class AuthController < ApplicationController
 
   def index
-
+    Tumblr.configure do |config|
+      config.consumer_key = ENV["TUMBLR_KEY"]
+      config.consumer_secret = ENV["TUMBLR_SECRET"]
+      config.oauth_token = "access_token"
+      config.oauth_token_secret = "access_token_secret"
+    end
   end
 end
