ec5c26bc4f97f0596bfda5d007956368a1a857aa
sun-vm <jmw686@nyu.edu>
2018-09-09 23:35:56 -0500

    add url to users, work on oauth 1 requests w/ sha1

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'devise'
 gem "figaro"
 gem 'omniauth-tumblr'
 gem 'httparty'
+# gem 'rickshaw'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,37 +1,46 @@
 class AuthController < ApplicationController
   include HTTParty
+  # DIGEST  = OpenSSL::Digest::Digest.new('sha1')
+  #
+  # Base64.encode64(OpenSSL::HMAC.digest(DIGEST,
+  #   "Secret Key", "Message"))
 
   def index
   end
 
   def show
-    # Authenticate via OAuth
-    # client = Tumblr::Client.new({
-    #   :consumer_key => 't8HPidBxWsBJiEqSulRQH8AYHE3TR0GJkX7UwTCeIo6A42oQiF',
-    #   :consumer_secret => 'tvVrypa7uiofA4ElpYQDda5fWaTqAdJdTUk5gtcUf0MMx2GCDx',
-    #   :oauth_token => 'bESAsGAMqukZGNn0brQ3gpNpgdSjo1EkFJ4CprZW43dBirfZaH',
-    #   :oauth_token_secret => '6EIqzdwTK9bERf7PIth0XP1JP6Nn5ZiFzxaACXbKsQSOEv4BoF'
-    # })
 
-    # Make the request
-    # client.likes
+    # render json:request.env["omniauth.auth"]
     current_user.access_token = params.fetch("oauth_token")
     current_user.access_secret = params.fetch("oauth_verifier")
+    current_user.url = request.env["omniauth.auth"].fetch("uid") + '.tumblr.com'
     current_user.save
 
-    # url = "api.tumblr.com/v2/blog/#{blog-identifier}/likes?api_key=#{ENV["TUMBLR_KEY"]}"
-    # res = HTTParty.get(url, {
-    #   headers: {"Authorization" => "Bearer " + current_user.access_token},
-    #   debug_output: STDOUT, # To show that User-Agent is Httparty
-    # })
-    render json:request.env["omniauth.auth"]
-    # Tumblr.configure do |config|
-    #   config.consumer_key = ENV["TUMBLR_KEY"]
-    #   config.consumer_secret = ENV["TUMBLR_SECRET"]
-    #   config.oauth_token = current_user.access_token
-    #   config.oauth_token_secret = current_user.access_secret
-    # end
-    # client = Tumblr::Client.new
-    # @likes = client.likes
+    url = "http://api.tumblr.com/v2/blog/#{current_user.url}/info?api_key=#{ENV["TUMBLR_KEY"]}"
+    # url = "http://api.tumblr.com/v2/user/likes?api_key=#{ENV["TUMBLR_KEY"]}"
+    # url = "http://api.tumblr.com/v2/user/info"
+    @res  = HTTParty.get(url)
+    sha = consumer_secret + "&" + token_secret + signature_base_string
+    sha = sha.to_sha1
+    req = "http://www.tumblr.com/oauth/request_token"
+    @res2 = HTTParty.get(req, {
+      # Authorization: OAuth realm="http://sp.example.com/",
+# oauth_consumer_key="0685bd9184jfhq22",
+# oauth_token="ad180jjd733klru7",
+# oauth_signature_method="HMAC-SHA1",
+# oauth_signature="wOJIO9A2W5mFwDgiDvZbTSMK%2FPY%3D",
+# oauth_timestamp="137131200",
+# oauth_nonce="4572616e48616d6d65724c61686176",
+# oauth_version="1.0"
+      headers: {"Authorization" =>
+        "oauth_cosumer_key=" + '"' + current_user.access_token + '"' + "," +
+        "oauth_token=" + '"' + current_user.access_token + '"' + "," +
+        'oauth_signature_method="HMAC-SHA1"' +
+        'oauth_signature="wOJIO9A2W5mFwDgiDvZbTSMK%2FPY%3D"' +
+        'oauth_timestamp="137131200"' +
+        'oauth_nonce="4572616e48616d6d65724c61686176"' +
+        'oauth_version="1.0" '
+      }
+      })
   end
 end
